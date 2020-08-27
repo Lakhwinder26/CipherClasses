8fc8fb6ae76d82c5606697cc4dee6b4d381a2d20
Lakhwinder Singh <lakhwinder0199@gmail.com>
2020-08-27 21:09:01 +0530

    Updates

---
+++ precache-manifest.fec9599224e699200b36743952d3013b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be5d89d0091ae7040a3a28fb1bc3d2e",
+    "url": "/CipherClasses/Portfolio/index.html"
+  },
+  {
+    "revision": "12140a85e27f9039d196",
+    "url": "/CipherClasses/Portfolio/static/css/main.1d9871c5.chunk.css"
+  },
+  {
+    "revision": "1185ee2726788c695dd4",
+    "url": "/CipherClasses/Portfolio/static/js/2.75abc4d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CipherClasses/Portfolio/static/js/2.75abc4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12140a85e27f9039d196",
+    "url": "/CipherClasses/Portfolio/static/js/main.cbcc60cd.chunk.js"
+  },
+  {
+    "revision": "f80e79d3ed652dafa537",
+    "url": "/CipherClasses/Portfolio/static/js/runtime-main.b9efca9a.js"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/CipherClasses/Portfolio/static/media/1.8f5ce275.png"
+  },
+  {
+    "revision": "5c570427ee23f69853d28aec805eee79",
+    "url": "/CipherClasses/Portfolio/static/media/2.5c570427.png"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/CipherClasses/Portfolio/static/media/4.0251d8ee.png"
+  },
+  {
+    "revision": "774352deeaa60ebe04164a1e28489771",
+    "url": "/CipherClasses/Portfolio/static/media/5.774352de.png"
+  },
+  {
+    "revision": "56d6c79b0181bf8fc40797d2e31fc952",
+    "url": "/CipherClasses/Portfolio/static/media/profile.56d6c79b.jpg"
+  }
+]);
